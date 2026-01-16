3ddda57453b0d9c1c3c4ecddfc2d0654694b0c1d
Elr1ck13 <renewcod13@outlook.com>
2026-01-15 20:36:01 -0600

    se conecta back con front de contactos y se centralizan URIs

---
+++ js/urls.js
@@ -0,0 +1,8 @@
+const BASE_URL = "http://localhost:8080";
+
+export const API_URLS = {
+  usuarios: `${BASE_URL}/api/usuarios`,
+  productos: `${BASE_URL}/api/productos`,
+  pedidos: `${BASE_URL}/api/pedidos`,
+  contactos: `${BASE_URL}/api/contactos`
+};
